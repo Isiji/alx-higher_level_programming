e589bcac8b5b9a42d82000b6d91ad9110377adfe
isiji <blairisiji@gmail.com>
2023-11-08 22:49:16 +0300

    additional

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_weighted_sum = 0
+    total_weights = 0
+
+    for score, weight in my_list:
+        total_weighted_sum += score * weight
+        total_weights += weight
+
+    if total_weights == 0:
+        return 0  # Avoid division by zero
+
+    return total_weighted_sum / total_weights
+
