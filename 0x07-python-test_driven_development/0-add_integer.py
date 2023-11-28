c4734e3f6069130d96d259c3c1ffc38a300d9542
isiji <blairisiji@gmail.com>
2023-11-28 11:19:21 +0300

    1st

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""function that adds integer"""
 def add_integer(a, b=98):
-    """returns the integer addition of a and b"""
+    """returns the integer addition of a and b
+    raise TypeError if a or b is non integer"""
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
