b445e01fda0ded0e79093f34764907de3f39d026
isiji <blairisiji@gmail.com>
2024-03-26 16:08:50 +0000

    lorum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-
 const fs = require('fs');
 const request = require('request');
-
-request.get(process.argv[2]).pipe(fs.writeFile(process.argv[3]));
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
