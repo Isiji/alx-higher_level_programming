2d4c4b22940e4ceaca5f7c3be576d316ef5fd4d5
isiji <blairisiji@gmail.com>
2024-04-02 15:23:24 +0300

    try

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
-curl -s -L "${1}"
+# Takes in a URL, sends a request to that URL, and displays the body of the response
+curl -sL "$1"
